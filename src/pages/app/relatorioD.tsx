1d62262f6b9affa068f77bbfb412859cef2b47cd
Marcos Monteiro <marcos.moliveira@outlook.com>
2024-09-11 15:15:07 -0300

    form create relatorio

--- src/pages/app/relatorioD.tsx
+++ src/pages/app/relatorioD.tsx
@@ -9,10 +9,21 @@ import {
     TableRow,
   } from "@/components/ui/table"
 
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+
 import button from '@/assets/criarUsuario.svg'
 import edit from '@/assets/Edit.svg'
 import trash from '@/assets/trash.svg'
 import { ScrollArea } from "@radix-ui/react-scroll-area";
+import { Input } from "@/components/ui/input";
+import { z } from 'zod'
+import { useForm } from "react-hook-form";
 
 const teste = [
     {
@@ -184,12 +195,63 @@ const teste = [
         Delete: "X"
     }
 ]
+const relatorioSchema = z.object({
+    data: z.date(),
+    codigoTarefa: z.number(),
+    idDocumento: z.string(),
+    quantidadeFolhas: z.string(),
+    horaInicio: z.string(),
+    horaTermino: z.string()
+})
+
+type RelatorioSchema = z.infer<typeof relatorioSchema>
 
 export function RelatorioDiario() {
+    const { register, handleSubmit, formState: { isSubmitted } } = useForm<RelatorioSchema>()
+
+    async function createActiveti(data: RelatorioSchema) {
+        
+    }
     return (
         <div className="flex flex-row justify-center items-center">
             <div className="flex flex-col p-5 items-end w-[95%] gap-5">
-                <Button variant={"ghost"} className="w-[3rem] p-0"><img src={button} alt="Criar nova atividade" /></Button>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button variant={"ghost"} className="w-[3rem] p-0"><img src={button} alt="Criar nova atividade" /></Button>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-[47rem] h-[30rem] flex flex-col justify-around items-center gap-4 py-4">
+                        <DialogHeader>
+                            <DialogTitle className="text-[2rem]">Cadastro de atividades</DialogTitle>
+                        </DialogHeader>
+                        <form onSubmit={handleSubmit(createActiveti)} className="grid grid-cols-3 gap-10 py-10">
+                            <div>
+                                <Input placeholder="Data" type="date" {...register('data')}></Input>
+                            </div>
+                            <div>
+                                <Input type="number" placeholder="Código de atividade" {...register('codigoTarefa')}></Input>
+                            </div>
+                            <div>
+                                <Input type="text" placeholder="Setor" readOnly></Input>
+                            </div>
+                            <div className="col-span-2">
+                                <Input type="text" placeholder="Descrição da atividade" readOnly></Input>
+                            </div>
+                            <div>
+                                <Input type="text" placeholder="Id Documento" {...register('idDocumento')}></Input>
+                            </div>
+                            <div>
+                                <Input type="text" placeholder="Quantidade de folhas" {...register('quantidadeFolhas')}></Input>
+                            </div>
+                            <div>
+                                <Input type="text" placeholder="Hora de inicio" {...register('horaInicio')}></Input>
+                            </div>
+                            <div>
+                                <Input type="text" placeholder="Hora de termino" {...register('horaTermino')}></Input>
+                            </div>
+                            <Button disabled={isSubmitted} type="submit" className="col-start-2">Salvar</Button>
+                        </form>
+                    </DialogContent>
+                </Dialog>
                 <ScrollArea className="h-[60vh] overflow-y-auto w-[95%] rounded-md border">
                     <Table className="text-center">
                         <TableHeader>
